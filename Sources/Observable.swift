75645f17094c03236e7933934dbb9c6f048af113
Eugene Tulusha <tulusheg@gmail.com>
2016-04-17 13:11:08 +0300

    Update Observable class for better Cartage support

--- Sources/Observable.swift
+++ Sources/Observable.swift
@@ -3,23 +3,23 @@ public class Observable<T> {
     let didChange = Event<(T, T)>()
     private var value: T
 
-    init(_ initialValue: T) {
+    public init(_ initialValue: T) {
         value = initialValue
     }
 
-    func set(newValue: T) {
+    public func set(newValue: T) {
         let oldValue = value
         value = newValue
         didChange.raise(oldValue, newValue)
     }
 
-    func get() -> T {
+    public func get() -> T {
         return value
     }
 }
 
 infix operator <- {}
 
-func <-<T> (property: Observable<T>, @autoclosure value: () -> T) {
+public func <-<T> (property: Observable<T>, @autoclosure value: () -> T) {
     property.set(value())
 }
